d6c396e10bac8dc585287abb4f5a8ceab41dd47a
phamyen592 <phamyen592@gmail.com>
2024-05-09 12:12:16 -0500

    TWA SDK integrated

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import { TonConnectButton } from "@tonconnect/ui-react";
 import { useMainContract } from "./hooks/useMainContract";
 import { useTonConnect } from "./hooks/useTonConnect";
 import { fromNano } from "@ton/core";
+import WebApp from "@twa-dev/sdk";
 
+const showAlert = () => {
+  WebApp.showAlert("Hey there!");
+};
 function App() {
+  
   const {
     contract_address,
     counter_value,
@@ -16,22 +21,36 @@ function App() {
   } = useMainContract();
   const { connected } = useTonConnect()
   return (
-    <>
+    <div>
       <div>
         <TonConnectButton />
       </div>
       <div>
         <div className='Card'>
+          <b>{WebApp.platform}</b>
           <b>Our contract Address</b>
           <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
           <b>Our contract Balance</b>
-          {contract_balance && <div className='Hint'>{fromNano(Number(contract_balance))}</div>}
+          {contract_balance && (
+            <div className='Hint'>{fromNano(contract_balance)}</div>
+          )}
         </div>
 
         <div className='Card'>
           <b>Counter Value</b>
           <div>{counter_value ?? "Loading..."}</div>
         </div>
+
+        <a
+          onClick={() => {
+            showAlert();
+          }}
+        >
+          Show Alert
+        </a>
+
+        <br />
+
         {connected && (
           <a
             onClick={() => {
@@ -41,29 +60,34 @@ function App() {
             Increment by 5
           </a>
         )}
+
         <br />
+
         {connected && (
           <a
             onClick={() => {
               sendDeposit();
             }}
           >
-            Deposit 1 TON
+            Request deposit of 1 TON
           </a>
         )}
+
         <br />
+
         {connected && (
           <a
             onClick={() => {
               sendWithdrawalRequest();
             }}
           >
-            Withdraw 0.7 TON
+            Request 0.7 TON withdrawal
           </a>
         )}
       </div>
-    </>
+    </div>
   );
+
 }
 
 export default App;
